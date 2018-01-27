8c77a7f4da416ba55d72e8e30a967753dff77f61
Curt Manning <cmann4ski@yahoo.com>
2018-01-26 17:13:29 -0800

    Added test case for countDigitsBeforeAndAfterDecimal

--- src/test/java/week2/day3/StringDemoTest.java
+++ src/test/java/week2/day3/StringDemoTest.java
@@ -2,7 +2,7 @@ package week2.day3;
 
 import org.testng.Assert;
 import org.testng.annotations.Test;
-//import week2.day3.appcode.StringDemo;
+
 public class StringDemoTest {
 	
 	@Test
@@ -10,7 +10,7 @@ public class StringDemoTest {
 
 		String inputString = "Hello Readers";
 		String searchString = "Hello";
-		System.out.println("\nStringDemoTest -> searchStringTest");
+		System.out.println("StringDemoTest -> searchStringTest");
 		int searchKey = 0;
 		int result = StringDemo.searchString(inputString,searchString);
 		Assert.assertEquals(result, searchKey);
@@ -21,8 +21,18 @@ public class StringDemoTest {
 
 		String inputString = "Welcome:to-java.com-hello:world";
 		String[] searchKey = {"Welcome:to","java.com","hello:world"};
-		System.out.println("\nStringDemoTest -> splitStringTest");
+		System.out.println("StringDemoTest -> splitStringTest");
 		String[] result = StringDemo.splitString(inputString);
+		Assert.assertEquals(result, searchKey);
+	}
+	
+	@Test
+	public void countDigitsBeforeAndAfterDecimalTest() {
+
+		double inputString = 143.32;
+		String searchKey = "Number of digits before decimal is 3 and after decimal is 2";
+		System.out.println("StringDemoTest -> countDigitsBeforeAndAfterDecimal");
+		String result = StringDemo.countDigitsBeforeAndAfterDecimal(inputString);
 		Assert.assertEquals(result, searchKey);
 	}
 }
